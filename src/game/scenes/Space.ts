2527ab9f2253a1fe83b8ab2d109414030bf1ce96
arthurteisseire <arthurteisseire33@gmail.com>
2024-06-15 20:00:20 +0200

    Fix player physics

--- src/game/scenes/Space.ts
+++ src/game/scenes/Space.ts
@@ -55,9 +55,9 @@ export class Space extends Phaser.Scene
     
     update(time: number, delta: number)
     {
-        this.updatePhysics(time, delta);
         this.background.update(delta);
         this.player.update(time, delta);
+        this.updatePhysics(time, delta);
         
         this.playerPositionText.setText(
             `Position: (${this.player.x.toFixed(2)}, ${this.player.y.toFixed(2)})`
@@ -68,14 +68,24 @@ export class Space extends Phaser.Scene
     {
         const delta_seconds: number = delta / 1000.0;
 
-        this.physics.accelerateToObject(this.player, this.planets[0], 400);
         if (this.cursors.up.isDown)
         {
+            this.physics.accelerateToObject(this.player, this.planets[0], 400);
             if (!this.player.oxygenTank.isEmpty())
             {
                 this.player.oxygenTank.consumeOxygen(this.player.oxygenBurstConsumptionBySecond * delta_seconds);
                 this.physics.velocityFromRotation(this.player.rotation, 200, this.player.body!.acceleration);
             }
+            else
+            {
+                this.player.setAcceleration(0);
+                this.physics.accelerateToObject(this.player, this.planets[0], 400);
+            }
+        }
+        else
+        {
+            this.player.setAcceleration(0);
+            this.physics.accelerateToObject(this.player, this.planets[0], 400);
         }
         
 
