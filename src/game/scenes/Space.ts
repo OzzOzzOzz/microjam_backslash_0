cc05a02fe0e3a25668b0ee6c2645694ad59b0751
arthurteisseire <arthurteisseire33@gmail.com>
2024-06-15 16:40:28 +0200

    Add player debug position

--- src/game/scenes/Space.ts
+++ src/game/scenes/Space.ts
@@ -6,7 +6,8 @@ export class Space extends Phaser.Scene
 {
     private player: Player;
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
-    spaceBackground: GameObjects.Image;
+    private spaceBackground: GameObjects.Image;
+    private playerPositionText: Phaser.GameObjects.Text;
     
     preload()
     {
@@ -19,6 +20,10 @@ export class Space extends Phaser.Scene
 
     create()
     {
+        const worldWidth = 2000;
+        const worldHeight = 2000;
+        this.physics.world.setBounds(0, 0, worldWidth, worldHeight);
+
         const screenHeight: number = this.sys.game.config.height as number;
         const screenWidth: number = this.sys.game.config.width as number;
         this.spaceBackground = this.add.image(
@@ -26,9 +31,13 @@ export class Space extends Phaser.Scene
             screenHeight / 2,
             'spaceBackground'
         );
+
         this.cursors = this.input.keyboard!.createCursorKeys();
         this.player = new Player(this, 400, 300, 'ship', this.cursors);
         this.cameras.main.startFollow(this.player);
+
+        this.playerPositionText = this.add.text(10, 10, '', { font: '16px Courier', color: '#ffffff' });
+        this.playerPositionText.setScrollFactor(0); // Ensure the text stays in the same place on the screen
 
         EventBus.emit('current-scene-ready', this);
     }
@@ -36,6 +45,10 @@ export class Space extends Phaser.Scene
     update(time: number, delta: number)
     {
         this.player.update(time, delta);
+        
+        this.playerPositionText.setText(
+            `Position: (${this.player.x.toFixed(2)}, ${this.player.y.toFixed(2)})`
+        );
     }
 
     changeScene ()
