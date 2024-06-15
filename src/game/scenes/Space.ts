b9d6d95fa82e5671bb7e960c76ba453b293c285c
arthurteisseire <arthurteisseire33@gmail.com>
2024-06-15 13:25:20 +0200

    Use width and height from config instead of hard-coded one

--- src/game/scenes/Space.ts
+++ src/game/scenes/Space.ts
@@ -1,6 +1,4 @@
 import { GameObjects, Scene } from 'phaser';
-import {EventBus} from "../EventBus.ts";
-
 import { EventBus } from '../EventBus';
 
 export class Space extends Scene
@@ -11,12 +9,18 @@ export class Space extends Scene
     constructor() {
         super('Space');
     }
-    
+
     create ()
     {
-          this.spaceBackground = this.add.image(1080 / 2, 720 / 2, 'spaceBackground');
-        EventBus.emit('current-scene-ready', this);
+        const screenWidth: number = this.sys.game.config.width as number;
+        const screenHeight: number = this.sys.game.config.height as number;
 
+        this.spaceBackground = this.add.image(
+            screenWidth / 2,
+            screenHeight / 2,
+              'spaceBackground'
+          );
+        EventBus.emit('current-scene-ready', this);
     }
 
     changeScene ()
