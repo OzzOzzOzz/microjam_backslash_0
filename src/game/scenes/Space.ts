6f17dc0d3a9813f4d174770a34a66ddae36506af
arthurteisseire <arthurteisseire33@gmail.com>
2024-06-15 13:44:24 +0200

    Add a moving square

--- src/game/scenes/Space.ts
+++ src/game/scenes/Space.ts
@@ -3,6 +3,8 @@ import { EventBus } from '../EventBus';
 
 export class Space extends Scene
 {
+    private player: Phaser.GameObjects.Graphics;
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     spaceBackground: GameObjects.Image;
 
 
@@ -12,15 +14,43 @@ export class Space extends Scene
 
     create ()
     {
-        const screenWidth: number = this.sys.game.config.width as number;
         const screenHeight: number = this.sys.game.config.height as number;
-
+        const screenWidth: number = this.sys.game.config.width as number;
         this.spaceBackground = this.add.image(
             screenWidth / 2,
             screenHeight / 2,
-              'spaceBackground'
-          );
+            'spaceBackground'
+        );
+
+        this.player = this.add.graphics();
+        this.player.fillStyle(0xff0000, 1);
+        this.player.fillRect(0, 0, 50, 50);
+        this.player.setPosition(100, 100);
+
+        this.cursors = this.input.keyboard!.createCursorKeys();
+        
         EventBus.emit('current-scene-ready', this);
+    }
+    
+    update(time: number, delta: number)
+    {
+        if (this.cursors.up.isDown)
+        {
+            this.player.y -= 10;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.y += 10;
+        }
+        else if (this.cursors.left.isDown)
+        {
+            this.player.x -= 10;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.x += 10;
+        }
+
     }
 
     changeScene ()
